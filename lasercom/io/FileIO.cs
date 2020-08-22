6fe0757b9d42c7c664f8542ccf61cc62f94573e6
Ignacio Lopez <ilopezp.chem@gmail.com>
2020-08-21 22:43:25 -0700

    csvhelper update

--- lasercom/io/FileIO.cs
+++ lasercom/io/FileIO.cs
@@ -42,38 +42,46 @@ namespace lasercom.io
         public static T[] ReadVector<T>(string FileName)
         {
             T[] vector;
-            var config = new CsvConfiguration
-            {
-                AllowComments = true,
-                HasHeaderRecord = false
-            };
             using (TextReader reader = File.OpenText(FileName))
-            using (CsvReader csv = new CsvReader(reader, config))
+            using (CsvReader csv = new CsvReader(reader, System.Globalization.CultureInfo.InvariantCulture))
+            {
+                csv.Configuration.AllowComments = true;
+                csv.Configuration.HasHeaderRecord = false;
                 vector = csv.GetRecords<T>().ToArray<T>();
+            }
             return vector;
         }
 
         public static void WriteVector<T>(string FileName, IEnumerable<T> Vector)
         {
-            TextWriter writer = new StreamWriter(FileName);
-            CsvWriter csv = new CsvWriter(writer);
-            csv.WriteRecords(Vector);
-            writer.Close();
+            using (var writer = new StreamWriter(FileName))
+            {
+                using (var csv = new CsvWriter(writer, System.Globalization.CultureInfo.InvariantCulture))
+                {
+                    csv.WriteRecords(Vector);
+                }
+                writer.Close();
+            }
         }
 
         public static void WriteMatrix<T>(string FileName, T[,] Matrix)
         {
-            TextWriter writer = new StreamWriter(FileName);
-            CsvWriter csv = new CsvWriter(writer);
-            for (int i = 0; i < Matrix.GetLength(0); i++)
+            using (var writer = new StreamWriter(FileName))
             {
-                for (int j = 0; j < Matrix.GetLength(1); j++)
+                using (var csv = new CsvWriter(writer, System.Globalization.CultureInfo.InvariantCulture))
                 {
-                    csv.WriteField(Matrix[i, j].ToString());
+                    for (int i = 0; i < Matrix.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < Matrix.GetLength(1); j++)
+                        {
+                            csv.WriteField(Matrix[i, j].ToString());
+                        }
+                        csv.NextRecord();
+                    }
                 }
-                csv.NextRecord();
+                writer.Close();
             }
-            writer.Close();
         }
     }
 }
+
